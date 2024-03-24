fbd9d68d7a1f26c90808df5f4fe45025f57dd7e5
RajKKapadia <raajforyou@gmail.com>
2024-03-24 16:18:19 +0530

    removed tables names from the folder

--- config.py
+++ config.py
@@ -10,13 +10,11 @@ MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_HOST = os.getenv('MYSQL_HOST')
 MYSQL_PORT = os.getenv('MYSQL_PORT')
 MYSQL_DB_NAME = os.getenv('MYSQL_DB_NAME')
-MYSQL_TABLES = []
 GPT_MODEL = os.getenv('GPT_MODEL')
 
 cwd = os.getcwd()
 
 DEFINITION_DIR = 'data_definition'
-CSV_DIR = 'csv_data'
 
 os.makedirs(
     os.path.join(
@@ -25,12 +23,5 @@ os.makedirs(
     ),
     exist_ok=True
 )
-
-for definition in os.listdir(CSV_DIR):
-    file_data = definition.rsplit('.', 1)
-    table_name = file_data[0]
-    extention = file_data[1]
-    if 'csv' in extention:
-        MYSQL_TABLES.append(table_name)
 
 ERROR_MESSAGE = 'We are facing an issue at this moment, please try after sometime.'
